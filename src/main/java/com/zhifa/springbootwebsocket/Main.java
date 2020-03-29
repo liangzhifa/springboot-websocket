9a656cbab44493f9460560abcf26000e32993848
xiaozhi止水 <877657682@qq.com>
2020-03-29 17:19:18 +0800

    Initial commit

---
+++ src/main/java/com/zhifa/springbootwebsocket/Main.java
@@ -0,0 +1,314 @@
+package com.zhifa.springbootwebsocket;
+
+import com.zhifa.springbootwebsocket.webSocket.WebSocket;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        WebSocket webSocket = new WebSocket();
+         Session session01=new Session() {
+             @Override
+             public WebSocketContainer getContainer() {
+                 return null;
+             }
+
+             @Override
+             public void addMessageHandler(MessageHandler messageHandler) throws IllegalStateException {
+
+             }
+
+             @Override
+             public Set<MessageHandler> getMessageHandlers() {
+                 return null;
+             }
+
+             @Override
+             public void removeMessageHandler(MessageHandler messageHandler) {
+
+             }
+
+             @Override
+             public String getProtocolVersion() {
+                 return null;
+             }
+
+             @Override
+             public String getNegotiatedSubprotocol() {
+                 return null;
+             }
+
+             @Override
+             public List<Extension> getNegotiatedExtensions() {
+                 return null;
+             }
+
+             @Override
+             public boolean isSecure() {
+                 return false;
+             }
+
+             @Override
+             public boolean isOpen() {
+                 return false;
+             }
+
+             @Override
+             public long getMaxIdleTimeout() {
+                 return 0;
+             }
+
+             @Override
+             public void setMaxIdleTimeout(long l) {
+
+             }
+
+             @Override
+             public void setMaxBinaryMessageBufferSize(int i) {
+
+             }
+
+             @Override
+             public int getMaxBinaryMessageBufferSize() {
+                 return 0;
+             }
+
+             @Override
+             public void setMaxTextMessageBufferSize(int i) {
+
+             }
+
+             @Override
+             public int getMaxTextMessageBufferSize() {
+                 return 0;
+             }
+
+             @Override
+             public RemoteEndpoint.Async getAsyncRemote() {
+                 return null;
+             }
+
+             @Override
+             public RemoteEndpoint.Basic getBasicRemote() {
+                 return null;
+             }
+
+             @Override
+             public String getId() {
+                 return null;
+             }
+
+             @Override
+             public void close() throws IOException {
+
+             }
+
+             @Override
+             public void close(CloseReason closeReason) throws IOException {
+
+             }
+
+             @Override
+             public URI getRequestURI() {
+                 return null;
+             }
+
+             @Override
+             public Map<String, List<String>> getRequestParameterMap() {
+                 return null;
+             }
+
+             @Override
+             public String getQueryString() {
+                 return null;
+             }
+
+             @Override
+             public Map<String, String> getPathParameters() {
+                 return null;
+             }
+
+             @Override
+             public Map<String, Object> getUserProperties() {
+                 return null;
+             }
+
+             @Override
+             public Principal getUserPrincipal() {
+                 return null;
+             }
+
+             @Override
+             public Set<Session> getOpenSessions() {
+                 return null;
+             }
+
+             @Override
+             public <T> void addMessageHandler(Class<T> aClass, MessageHandler.Partial<T> partial) throws IllegalStateException {
+
+             }
+
+             @Override
+             public <T> void addMessageHandler(Class<T> aClass, MessageHandler.Whole<T> whole) throws IllegalStateException {
+
+             }
+         };
+         Session session02=new Session() {
+             @Override
+             public WebSocketContainer getContainer() {
+                 return null;
+             }
+
+             @Override
+             public void addMessageHandler(MessageHandler messageHandler) throws IllegalStateException {
+
+             }
+
+             @Override
+             public Set<MessageHandler> getMessageHandlers() {
+                 return null;
+             }
+
+             @Override
+             public void removeMessageHandler(MessageHandler messageHandler) {
+
+             }
+
+             @Override
+             public String getProtocolVersion() {
+                 return null;
+             }
+
+             @Override
+             public String getNegotiatedSubprotocol() {
+                 return null;
+             }
+
+             @Override
+             public List<Extension> getNegotiatedExtensions() {
+                 return null;
+             }
+
+             @Override
+             public boolean isSecure() {
+                 return false;
+             }
+
+             @Override
+             public boolean isOpen() {
+                 return false;
+             }
+
+             @Override
+             public long getMaxIdleTimeout() {
+                 return 0;
+             }
+
+             @Override
+             public void setMaxIdleTimeout(long l) {
+
+             }
+
+             @Override
+             public void setMaxBinaryMessageBufferSize(int i) {
+
+             }
+
+             @Override
+             public int getMaxBinaryMessageBufferSize() {
+                 return 0;
+             }
+
+             @Override
+             public void setMaxTextMessageBufferSize(int i) {
+
+             }
+
+             @Override
+             public int getMaxTextMessageBufferSize() {
+                 return 0;
+             }
+
+             @Override
+             public RemoteEndpoint.Async getAsyncRemote() {
+                 return null;
+             }
+
+             @Override
+             public RemoteEndpoint.Basic getBasicRemote() {
+                 return null;
+             }
+
+             @Override
+             public String getId() {
+                 return null;
+             }
+
+             @Override
+             public void close() throws IOException {
+
+             }
+
+             @Override
+             public void close(CloseReason closeReason) throws IOException {
+
+             }
+
+             @Override
+             public URI getRequestURI() {
+                 return null;
+             }
+
+             @Override
+             public Map<String, List<String>> getRequestParameterMap() {
+                 return null;
+             }
+
+             @Override
+             public String getQueryString() {
+                 return null;
+             }
+
+             @Override
+             public Map<String, String> getPathParameters() {
+                 return null;
+             }
+
+             @Override
+             public Map<String, Object> getUserProperties() {
+                 return null;
+             }
+
+             @Override
+             public Principal getUserPrincipal() {
+                 return null;
+             }
+
+             @Override
+             public Set<Session> getOpenSessions() {
+                 return null;
+             }
+
+             @Override
+             public <T> void addMessageHandler(Class<T> aClass, MessageHandler.Partial<T> partial) throws IllegalStateException {
+
+             }
+
+             @Override
+             public <T> void addMessageHandler(Class<T> aClass, MessageHandler.Whole<T> whole) throws IllegalStateException {
+
+             }
+         };
+        webSocket.setSession(session01);
+        webSocket.setSession(session02);
+
+        System.out.println();
+
+    }
+}
